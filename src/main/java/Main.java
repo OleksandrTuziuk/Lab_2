5b4ae888bfebc0553cb78ea2c0b68df22225f83b
OleksandrTuziuk <tuziuk.o.s.-io53@edu.kpi.ua>
2026-02-21 15:48:09 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,20 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
-    static void main(String[] args){
-        String string = sc.nextLine();
+    static void main(String[] args) {
+        System.out.println("Введіть текст: ");
+        final String string = sc.nextLine();
+        while (true) {
+            System.out.println("Введіть символ за яким буде відбуватись сортування: ");
+            final String letter = sc.nextLine();
+            if (letter.length() > 1) {
+                System.out.println("Введено декілька символів");
+                continue;
+            }
+            break;
+        }
 
-        Text text = new Text("banana myth aardvark catalog drama anaconda sky panorama caramel alpha extravaganza lamp");
+        Text text = new Text(string);
 
         System.out.println(text.sort('a'));
     }
